6209e28f4b3c59eb3f39c6da2ecfd2ba434056e0
Majercsik73 <riasztotnekem@gmail.com>
2021-10-11 17:28:58 +0200

    Add project files.

---
+++ Szamologep/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace Szamologep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.egy = new System.Windows.Forms.Button();
+            this.ketto = new System.Windows.Forms.Button();
+            this.harom = new System.Windows.Forms.Button();
+            this.negy = new System.Windows.Forms.Button();
+            this.ot = new System.Windows.Forms.Button();
+            this.hat = new System.Windows.Forms.Button();
+            this.het = new System.Windows.Forms.Button();
+            this.nyolc = new System.Windows.Forms.Button();
+            this.kilenc = new System.Windows.Forms.Button();
+            this.nulla = new System.Windows.Forms.Button();
+            this.egyenlo = new System.Windows.Forms.Button();
+            this.plusz = new System.Windows.Forms.Button();
+            this.minusz = new System.Windows.Forms.Button();
+            this.szor = new System.Windows.Forms.Button();
+            this.oszt = new System.Windows.Forms.Button();
+            this.Lbevitel = new System.Windows.Forms.Label();
+            this.clear = new System.Windows.Forms.Button();
+            this.Belojel = new System.Windows.Forms.Button();
+            this.Btizedes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // egy
+            // 
+            this.egy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.egy.Location = new System.Drawing.Point(138, 240);
+            this.egy.Name = "egy";
+            this.egy.Size = new System.Drawing.Size(75, 44);
+            this.egy.TabIndex = 1;
+            this.egy.Text = "1";
+            this.egy.UseVisualStyleBackColor = true;
+            this.egy.Click += new System.EventHandler(this.egy_Click);
+            // 
+            // ketto
+            // 
+            this.ketto.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ketto.Location = new System.Drawing.Point(245, 238);
+            this.ketto.Name = "ketto";
+            this.ketto.Size = new System.Drawing.Size(75, 45);
+            this.ketto.TabIndex = 2;
+            this.ketto.Text = "2";
+            this.ketto.UseVisualStyleBackColor = true;
+            this.ketto.Click += new System.EventHandler(this.ketto_Click);
+            // 
+            // harom
+            // 
+            this.harom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.harom.Location = new System.Drawing.Point(353, 239);
+            this.harom.Name = "harom";
+            this.harom.Size = new System.Drawing.Size(75, 44);
+            this.harom.TabIndex = 3;
+            this.harom.Text = "3";
+            this.harom.UseVisualStyleBackColor = true;
+            this.harom.Click += new System.EventHandler(this.harom_Click);
+            // 
+            // negy
+            // 
+            this.negy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.negy.Location = new System.Drawing.Point(138, 170);
+            this.negy.Name = "negy";
+            this.negy.Size = new System.Drawing.Size(75, 45);
+            this.negy.TabIndex = 4;
+            this.negy.Text = "4";
+            this.negy.UseVisualStyleBackColor = true;
+            this.negy.Click += new System.EventHandler(this.negy_Click);
+            // 
+            // ot
+            // 
+            this.ot.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ot.Location = new System.Drawing.Point(245, 170);
+            this.ot.Name = "ot";
+            this.ot.Size = new System.Drawing.Size(75, 45);
+            this.ot.TabIndex = 5;
+            this.ot.Text = "5";
+            this.ot.UseVisualStyleBackColor = true;
+            this.ot.Click += new System.EventHandler(this.ot_Click);
+            // 
+            // hat
+            // 
+            this.hat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hat.Location = new System.Drawing.Point(353, 170);
+            this.hat.Name = "hat";
+            this.hat.Size = new System.Drawing.Size(75, 45);
+            this.hat.TabIndex = 6;
+            this.hat.Text = "6";
+            this.hat.UseVisualStyleBackColor = true;
+            this.hat.Click += new System.EventHandler(this.hat_Click);
+            // 
+            // het
+            // 
+            this.het.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.het.Location = new System.Drawing.Point(138, 106);
+            this.het.Name = "het";
+            this.het.Size = new System.Drawing.Size(75, 45);
+            this.het.TabIndex = 7;
+            this.het.Text = "7";
+            this.het.UseVisualStyleBackColor = true;
+            this.het.Click += new System.EventHandler(this.het_Click);
+            // 
+            // nyolc
+            // 
+            this.nyolc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nyolc.Location = new System.Drawing.Point(245, 106);
+            this.nyolc.Name = "nyolc";
+            this.nyolc.Size = new System.Drawing.Size(75, 45);
+            this.nyolc.TabIndex = 8;
+            this.nyolc.Text = "8";
+            this.nyolc.UseVisualStyleBackColor = true;
+            this.nyolc.Click += new System.EventHandler(this.nyolc_Click);
+            // 
+            // kilenc
+            // 
+            this.kilenc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.kilenc.Location = new System.Drawing.Point(353, 106);
+            this.kilenc.Name = "kilenc";
+            this.kilenc.Size = new System.Drawing.Size(75, 45);
+            this.kilenc.TabIndex = 9;
+            this.kilenc.Text = "9";
+            this.kilenc.UseVisualStyleBackColor = true;
+            this.kilenc.Click += new System.EventHandler(this.kilenc_Click);
+            // 
+            // nulla
+            // 
+            this.nulla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.nulla.Location = new System.Drawing.Point(245, 306);
+            this.nulla.Name = "nulla";
+            this.nulla.Size = new System.Drawing.Size(75, 43);
+            this.nulla.TabIndex = 10;
+            this.nulla.Text = "0";
+            this.nulla.UseVisualStyleBackColor = true;
+            this.nulla.Click += new System.EventHandler(this.nulla_Click);
+            // 
+            // egyenlo
+            // 
+            this.egyenlo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.egyenlo.Location = new System.Drawing.Point(467, 371);
+            this.egyenlo.Name = "egyenlo";
+            this.egyenlo.Size = new System.Drawing.Size(75, 43);
+            this.egyenlo.TabIndex = 11;
+            this.egyenlo.Text = "=";
+            this.egyenlo.UseVisualStyleBackColor = true;
+            this.egyenlo.Click += new System.EventHandler(this.egyenlo_Click);
+            // 
+            // plusz
+            // 
+            this.plusz.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.plusz.Location = new System.Drawing.Point(467, 107);
+            this.plusz.Name = "plusz";
+            this.plusz.Size = new System.Drawing.Size(75, 44);
+            this.plusz.TabIndex = 12;
+            this.plusz.Text = "+";
+            this.plusz.UseVisualStyleBackColor = true;
+            this.plusz.Click += new System.EventHandler(this.plusz_Click);
+            // 
+            // minusz
+            // 
+            this.minusz.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.minusz.Location = new System.Drawing.Point(467, 171);
+            this.minusz.Name = "minusz";
+            this.minusz.Size = new System.Drawing.Size(75, 44);
+            this.minusz.TabIndex = 13;
+            this.minusz.Text = "--";
+            this.minusz.UseVisualStyleBackColor = true;
+            this.minusz.Click += new System.EventHandler(this.minusz_Click);
+            // 
+            // szor
+            // 
+            this.szor.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.szor.Location = new System.Drawing.Point(466, 238);
+            this.szor.Name = "szor";
+            this.szor.Size = new System.Drawing.Size(75, 43);
+            this.szor.TabIndex = 14;
+            this.szor.Text = "x";
+            this.szor.UseVisualStyleBackColor = true;
+            this.szor.Click += new System.EventHandler(this.szor_Click);
+            // 
+            // oszt
+            // 
+            this.oszt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.oszt.Location = new System.Drawing.Point(466, 306);
+            this.oszt.Name = "oszt";
+            this.oszt.Size = new System.Drawing.Size(75, 42);
+            this.oszt.TabIndex = 15;
+            this.oszt.Text = "/";
+            this.oszt.UseVisualStyleBackColor = true;
+            this.oszt.Click += new System.EventHandler(this.oszt_Click);
+            // 
+            // Lbevitel
+            // 
+            this.Lbevitel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Lbevitel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbevitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Lbevitel.Location = new System.Drawing.Point(138, 34);
+            this.Lbevitel.Name = "Lbevitel";
+            this.Lbevitel.Size = new System.Drawing.Size(403, 53);
+            this.Lbevitel.TabIndex = 18;
+            this.Lbevitel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.clear.Location = new System.Drawing.Point(138, 371);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 42);
+            this.clear.TabIndex = 19;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Belojel
+            // 
+            this.Belojel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Belojel.Location = new System.Drawing.Point(138, 306);
+            this.Belojel.Name = "Belojel";
+            this.Belojel.Size = new System.Drawing.Size(75, 42);
+            this.Belojel.TabIndex = 20;
+            this.Belojel.Text = "+/-";
+            this.Belojel.UseVisualStyleBackColor = true;
+            this.Belojel.Click += new System.EventHandler(this.Belojel_Click);
+            // 
+            // Btizedes
+            // 
+            this.Btizedes.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Btizedes.Location = new System.Drawing.Point(353, 306);
+            this.Btizedes.Name = "Btizedes";
+            this.Btizedes.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Btizedes.Size = new System.Drawing.Size(75, 42);
+            this.Btizedes.TabIndex = 21;
+            this.Btizedes.Text = ",";
+            this.Btizedes.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Btizedes.UseVisualStyleBackColor = true;
+            this.Btizedes.Click += new System.EventHandler(this.Btizedes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 485);
+            this.Controls.Add(this.Btizedes);
+            this.Controls.Add(this.Belojel);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.Lbevitel);
+            this.Controls.Add(this.oszt);
+            this.Controls.Add(this.szor);
+            this.Controls.Add(this.minusz);
+            this.Controls.Add(this.plusz);
+            this.Controls.Add(this.egyenlo);
+            this.Controls.Add(this.nulla);
+            this.Controls.Add(this.kilenc);
+            this.Controls.Add(this.nyolc);
+            this.Controls.Add(this.het);
+            this.Controls.Add(this.hat);
+            this.Controls.Add(this.ot);
+            this.Controls.Add(this.negy);
+            this.Controls.Add(this.harom);
+            this.Controls.Add(this.ketto);
+            this.Controls.Add(this.egy);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Számológép";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button egy;
+        private System.Windows.Forms.Button ketto;
+        private System.Windows.Forms.Button harom;
+        private System.Windows.Forms.Button negy;
+        private System.Windows.Forms.Button ot;
+        private System.Windows.Forms.Button hat;
+        private System.Windows.Forms.Button het;
+        private System.Windows.Forms.Button nyolc;
+        private System.Windows.Forms.Button kilenc;
+        private System.Windows.Forms.Button nulla;
+        private System.Windows.Forms.Button egyenlo;
+        private System.Windows.Forms.Button plusz;
+        private System.Windows.Forms.Button minusz;
+        private System.Windows.Forms.Button szor;
+        private System.Windows.Forms.Button oszt;
+        private System.Windows.Forms.Label Lbevitel;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button Belojel;
+        private System.Windows.Forms.Button Btizedes;
+    }
+}
+
---
+++ Szamologep/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szamologep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool ujszame = true;
+
+        private void egy_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "1";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "1";
+                ujszame = true;
+            }
+        }
+
+        private void ketto_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "2";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "2";
+                ujszame = true;
+            }
+        }
+
+        private void harom_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "3";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "3";
+                ujszame = true;
+            }
+        }
+
+        private void nulla_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "0";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "0";
+                ujszame = true;
+            }
+        }
+
+        private void negy_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "4";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "4";
+                ujszame = true;
+            }
+        }
+
+        private void ot_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "5";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "5";
+                ujszame = true;
+            }
+        }
+
+        private void hat_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "6";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "6";
+                ujszame = true;
+            }
+        }
+
+        private void het_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "7";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "7";
+                ujszame = true;
+            }
+        }
+
+        private void nyolc_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "8";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "8";
+                ujszame = true;
+            }
+        }
+
+        private void kilenc_Click(object sender, EventArgs e)
+        {
+            if (ujszame) Lbevitel.Text += "9";
+            else
+            {
+                Lbevitel.Text = "";
+                Lbevitel.Text = "9";
+                ujszame = true;
+            }
+        }
+
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double d = 0;
+        double f = 0;
+        double g = 0;
+        bool eloszorszamol = true;
+        double j = 0;
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            Lbevitel.Text = "";
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            f = 0;
+            g = 0;
+            eloszorszamol = true;
+            j = 0;
+            ujszame = true;
+
+        }
+
+        public void plusz_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            eloszorszamol = true;
+            a = Convert.ToDouble(Lbevitel.Text);
+            Lbevitel.Text = "";
+        }
+
+        private void minusz_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            eloszorszamol = true;
+            b = Convert.ToDouble(Lbevitel.Text);
+            Lbevitel.Text = "";
+        }
+
+        private void szor_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            eloszorszamol = true;
+            c = Convert.ToDouble(Lbevitel.Text);
+            Lbevitel.Text = "";
+        }
+
+        private void oszt_Click(object sender, EventArgs e)
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            d = 0;
+            eloszorszamol = true;
+            d = Convert.ToDouble(Lbevitel.Text);
+            Lbevitel.Text = "";
+        }
+
+        private void egyenlo_Click(object sender, EventArgs e)
+        {
+            if (eloszorszamol) EloszorSzamol();
+            
+            else TovabbiSzamol();
+
+            ujszame = false;
+        }
+
+        private void EloszorSzamol()
+        {
+            f = Convert.ToDouble(Lbevitel.Text);
+            Lbevitel.Text = "";
+            if (a != 0)
+            {
+                g = a + f;
+                Lbevitel.Text = g.ToString();
+            }
+            else if (b != 0)
+            {
+                g = b - f;
+                Lbevitel.Text = g.ToString();
+            }
+            else if (c != 0)
+            {
+                g = c * f;
+                Lbevitel.Text = g.ToString();
+            }
+            else
+            {
+                g = d / f;
+                Lbevitel.Text = g.ToString();
+            }
+            eloszorszamol = false;
+        }
+
+        private void TovabbiSzamol()
+        {
+            j = Convert.ToDouble(Lbevitel.Text);
+            if (a != 0)
+            {
+                g = j + f;
+                Lbevitel.Text = g.ToString();
+            }
+            else if (b != 0)
+            {
+                g = j - f;
+                Lbevitel.Text = g.ToString();
+            }
+            else if (c != 0)
+            {
+                g = j * f;
+                Lbevitel.Text = g.ToString();
+            }
+            else
+            {
+                g = j / f;
+                Lbevitel.Text = g.ToString();
+            }
+        }
+
+        private void Belojel_Click(object sender, EventArgs e)
+        {
+            if (Lbevitel.Text == "") Lbevitel.Text += "-";
+
+            else if (Lbevitel.Text == "-") Lbevitel.Text = "";
+        }
+
+        private void Btizedes_Click(object sender, EventArgs e)
+        {
+            if (Lbevitel.Text == "") Lbevitel.Text += "0,";
+
+            else Lbevitel.Text += ",";
+        }
+    }
+}
